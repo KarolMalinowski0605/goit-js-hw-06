b5ed1d2941e1de1aaf0f567fe22dfc45e8429aea
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-07-27 19:20:43 +0200

    task-02

--- js/task-01.js
+++ js/task-01.js
@@ -6,11 +6,11 @@ for (let i = 0; i < items.length; i++){
 
     const title = items[i].querySelector("h2").textContent;
     console.log( 'Category:' ,title);
+    
     const allItems = items[i].querySelectorAll("li");
     
     const numberElements = allItems.length;
     console.log('Elements:',numberElements);
-
 }
 
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+
+for(let i = 0; i < ingredients.length; i += 1){
+ 
+  const newElementLi = document.createElement('li');
+  
+
+  newElementLi.textContent = ingredients[i];
+  newElementLi.classList.add('item');
+  listIngredients.append(newElementLi);
+
+}
+
+console.log(listIngredients);
