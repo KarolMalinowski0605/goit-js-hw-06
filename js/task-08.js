0584c2c87ee80fbec913227c3720c97329b9514e
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-08-03 22:49:53 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+
+const form = document.querySelector(".login-form")
+        form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    const emailInput = form.elements.email;
+    const passwordInput = form.elements.password;
+
+    if (emailInput.value === "" || passwordInput.value === "") {
+        return alert ("Please fill in all the fields!");
+    }
+
+const frmUser = {
+    email: emailInput.value,
+    password: passwordInput.value
+  };
+  console.log(frmUser);
+
+  form.reset();
+  
+}
