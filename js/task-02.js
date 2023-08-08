c50321120cce4c180c66b9c577163372038c2c88
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-08-08 16:26:53 +0200

    correct task-02

--- js/task-02.js
+++ js/task-02.js
@@ -8,16 +8,20 @@ const ingredients = [
 ];
 
 const listIngredients = document.querySelector('#ingredients');
+const elementsArray = [];
 
 for(let i = 0; i < ingredients.length; i += 1){
  
   const newElementLi = document.createElement('li');
   
-
   newElementLi.textContent = ingredients[i];
   newElementLi.classList.add('item');
-  listIngredients.append(newElementLi);
+
+  
+  elementsArray.push(newElementLi);
 
 }
+
+listIngredients.append(...elementsArray);
 
 console.log(listIngredients);
